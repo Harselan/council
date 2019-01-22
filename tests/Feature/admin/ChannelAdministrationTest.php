3b901c03c50c0a5a6929eb71fa98e26183297514
Harald <harald@orgotech.com>
2019-01-22 11:12:56 +0100

    Refactor test

--- tests/Feature/admin/ChannelAdministrationTest.php
+++ tests/Feature/admin/ChannelAdministrationTest.php
@@ -95,17 +95,12 @@ class ChannelAdministrationTest extends TestCase
 	}
 
 	/** @test */
-	public function archived_channels_should_not_influence_existing_thread()
+	public function the_path_to_a_channel_is_unaffected_by_its_archived_status()
 	{
-		$this->signInAdmin();
+		$thread = create( 'App\Thread' );
+		$path   = $thread->path();
 
-		$channel = create( 'App\Channel' );
-		$thread = create( 'App\Thread', [ 'channel_id' => $channel->id ] );
-		$path = $thread->path();
-
-		$channel->update([
-			'archived' => true
-		]);
+		$thread->channel->archive();
 
 		$this->assertEquals( $path, $thread->fresh()->path() );
 	}
