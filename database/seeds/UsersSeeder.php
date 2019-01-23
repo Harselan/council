fdaef0033ab9b6269e236ba1e8b154c6e93916b9
Harald <harald@orgotech.com>
2019-01-23 10:55:31 +0100

    Updated seeders for better testing data

--- database/seeds/UsersSeeder.php
+++ database/seeds/UsersSeeder.php
@@ -12,14 +12,47 @@ class UsersSeeder extends Seeder
      */
     public function run()
     {
+	    Schema::disableForeignKeyConstraints();
+
         User::truncate();
 
-        factory( User::class )
-            ->create([
-            	'name' => 'John Doe',
-	            'username' => 'johndoe',
-	            'email' => 'john@example.com',
-	            'password' => bcrypt('password'),
-            ]);
+        collect( [
+	        [
+		        'name' => 'John Doe',
+		        'username' => 'johndoe',
+		        'email' => 'john@example.com',
+		        'password' => bcrypt('password'),
+	        ],
+	        [
+		        'name' => 'Indiana Jones',
+		        'username' => 'rotla1981',
+		        'email' => 'indy@example.com',
+		        'password' => bcrypt('password')
+	        ],
+	        [
+		        'name' => 'Ben Solo',
+		        'username' => 'KyloRen',
+		        'email' => 'kylo@example.com',
+		        'password' => bcrypt('password')
+	        ],
+	        [
+		        'name' => 'Marty McFly',
+		        'username' => '121gigawatts',
+		        'email' => 'calvin@example.com',
+		        'password' => bcrypt('password')
+	        ]
+        ] )->each( function( $user )
+        {
+	        factory( User::class )->create(
+		        [
+			        'name'      => $user['name'],
+			        'username'  => $user['username'],
+			        'email'     => $user['email'],
+			        'password'  => bcrypt('password')
+		        ]
+	        );
+        } );
+
+	    Schema::enableForeignKeyConstraints();
     }
 }
